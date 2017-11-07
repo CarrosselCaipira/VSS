30d55316fa7600d78ea9ef6ecebe18e9b04a2ed7
Danilo Weber Nunes <danilo@localhost.localdomain>
2017-11-07 13:59:16 -0200

    Programa funcionando em condição de teste.

--- MAIN.cpp
+++ MAIN.cpp
@@ -8,17 +8,21 @@
 int main() {
 	std::vector<Robo> robosTime(3);
 	std::vector<Robo> robosTimeAdv(3);
+	std::vector<posXY> obstaculosCampo(3);
 
-	Camera cam(1);
+	Camera cam(0);
 	Radio radio(robosTime);
-	CPH campoPotencial1(robosTime[0], robosTimeAdv);
-	CPH campoPotencial2(robosTime[1], robosTimeAdv);
-	CPH campoPotencial3(robosTime[2], robosTimeAdv);
+	CPH campoPotencial1(robosTime[0], obstaculosCampo);
+	CPH campoPotencial2(robosTime[1], obstaculosCampo);
+	CPH campoPotencial3(robosTime[2], obstaculosCampo);
 
 	// setando os roteiros
 	robosTime[0].setRoteiro(GOLEIRO_BASICO);
 	robosTime[1].setRoteiro(VOLANTE_BASICO);
 	robosTime[2].setRoteiro(ATACANTE_BASICO);
+
+	cv::Mat Frame;
+
 
 	while(true) {
 		// pegando as informacoes de localizacao a partir da camera para os robos e a bola
@@ -27,29 +31,39 @@ int main() {
 		// procurando pela bola
 		cam.setCorObjeto(ALARANJADO);
 		cam.getPosicaoAtualObjeto(bola);
+
+		/* BEGIN DEBUG */
+	  std::cout << " bola: " << "x: " << bola.x << " y: "<< bola.y;
+		Frame = cam.getFrameOriginalRecortado();
+		cv::Point centroCirculo(bola.x, bola.y);
+		int raioCirculo = 5;
+		cv::circle(Frame, centroCirculo, raioCirculo, cv::Scalar(0, 255, 0), 5);
+		cv::imshow("FrameOriginal Robo Detectado", Frame);
+		/* END DEBUG */
+
 		// procurando pelos robos aliados
-		cam.setCorObjeto(CORES);
+		cam.setCorObjeto(INDEFINIDA);
 		cam.getPosicaoAtualObjeto(localObjeto);
 		robosTime[0].setPosicaoAtualRobo(localObjeto);
 
-		cam.setCorObjeto(CORES);
+		cam.setCorObjeto(INDEFINIDA);
 		cam.getPosicaoAtualObjeto(localObjeto);
 		robosTime[1].setPosicaoAtualRobo(localObjeto);
 
-		cam.setCorObjeto(CORES);
+		cam.setCorObjeto(INDEFINIDA);
 		cam.getPosicaoAtualObjeto(localObjeto);
 		robosTime[2].setPosicaoAtualRobo(localObjeto);
 
 		// procurando pelos robos adversarios
-		cam.setCorObjeto(CORES);
+		cam.setCorObjeto(INDEFINIDA);
 		cam.getPosicaoAtualObjeto(localObjeto);
 		robosTimeAdv[0].setPosicaoAtualRobo(localObjeto);
 
-		cam.setCorObjeto(CORES);
+		cam.setCorObjeto(INDEFINIDA);
 		cam.getPosicaoAtualObjeto(localObjeto);
 		robosTimeAdv[1].setPosicaoAtualRobo(localObjeto);
 
-		cam.setCorObjeto(CORES);
+		cam.setCorObjeto(INDEFINIDA);
 		cam.getPosicaoAtualObjeto(localObjeto);
 		robosTimeAdv[2].setPosicaoAtualRobo(localObjeto);
 
@@ -57,6 +71,10 @@ int main() {
 		robosTime[0].run();
 		robosTime[1].run();
 		robosTime[2].run();
+
+		// transformando o vetor de robos em vetor de obstaculos (posXY)
+		for(int i = 0; i < robosTimeAdv.size(); i++)
+			obstaculosCampo[i] = robosTimeAdv[i].getPosicaoAtualRobo();
 
 		// calculando velocidade das rodas para o robo 1
 		campoPotencial1.calculaVelRodas();
