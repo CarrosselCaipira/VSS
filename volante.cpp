8c4aa9207f1aea1b174c57fe5ac7a1c86da4999b
Danilo Weber Nunes <danilownunes@gmail.com>
2018-09-30 19:07:01 -0300

    Alteração da função de 'getAtacantePosse' para 'isAtacantePosse' visto que esta retorna um booleano.

--- volante.cpp
+++ volante.cpp
@@ -10,36 +10,36 @@ void Roteiro::volante(Robo& r, posXY& b) {
 
 
     // O atacante possui a posse da bola
-    if (Tecnico.getAtacantePosse()) {
-        
+    if (Tecnico.isAtacantePosse()) {
+
         // O volante se encontra ate 10cm atras do atacante e
         // o volante esta mais proximo do meio de campo do que o atacante
         // "Caso A" -> se posicionara a frente do atacante para escolta-lo
         // (posVolante = posAtacante.X + TAM_ROBO)
-        // (posVolante = posAtacante.y (+-) TAM_ROBO). O volante se posiciona em Y mais proximo de meio de campo        
-        if ((std::abs(r.getPosicaoAtualRobo().x - Tecnico.getPosAtualAtacante().x) <= 10) 
+        // (posVolante = posAtacante.y (+-) TAM_ROBO). O volante se posiciona em Y mais proximo de meio de campo
+        if ((std::abs(r.getPosicaoAtualRobo().x - Tecnico.getPosAtualAtacante().x) <= 10)
             && (std::abs(TAM_Y_CAMPO/2 - r.getPosicaoAtualRobo().y) < std::abs(TAM_Y_CAMPO/2 - Tecnico.getPosAtualAtacante().y))) {
-            
+
             if(r.getPosicaoAtualRobo().y <= TAM_Y_AREA_GOL / 2) {
-                // Volante esta abaixo do meio de campo em Y                    
-                r.setPosicaoObj(Tecnico.getPosAtualAtacante().x + TAM_ROBO, Tecnico.getPosAtualAtacante().Y + TAM_ROBO);                                                                                                                                                                    
+                // Volante esta abaixo do meio de campo em Y
+                r.setPosicaoObj(Tecnico.getPosAtualAtacante().x + TAM_ROBO, Tecnico.getPosAtualAtacante().Y + TAM_ROBO);
             }
-            
+
             else{
                 // Volante esta acima do meio de campo em Y
-                r.setPosicaoObj(Tecnico.getPosAtualAtacante().x + TAM_ROBO, Tecnico.getPosAtualAtacante().Y - TAM_ROBO);                                                
+                r.setPosicaoObj(Tecnico.getPosAtualAtacante().x + TAM_ROBO, Tecnico.getPosAtualAtacante().Y - TAM_ROBO);
             }
         }
 
         // O volante se encontra ate 10 cm atras do atacante e esta mais perto da lateral respectiva
-        // ou o volante se encontra mais de 10cm atras do atacante                
+        // ou o volante se encontra mais de 10cm atras do atacante
         // "Caso B" ou "Caso C", respectivamente
         else {
-            // O volante segue o atacante 20cm atras           
-            r.setPosicaoObj(Tecnico.getPosAtualAtacante().x - 20, Tecnico.getPosAtualAtacante().Y);                                                
+            // O volante segue o atacante 20cm atras
+            r.setPosicaoObj(Tecnico.getPosAtualAtacante().x - 20, Tecnico.getPosAtualAtacante().Y);
         }
     }
-    
+
     // O atacante nao tem a posse da bola
     else {
         // O volante esta em posse da bola
@@ -53,9 +53,9 @@ void Roteiro::volante(Robo& r, posXY& b) {
             // A bola esta no campo adversario
             if (b.isInCampoAdv()){
                 Comportamento::posicionaPosBola(r, b);
-                                                
+
             }
-    
+
             // Se a bola esta no nosso campo
             else {
 
@@ -66,9 +66,9 @@ void Roteiro::volante(Robo& r, posXY& b) {
                     r.setPosicaoObjX(TAM_X_AREA_GOL + TAM_ROBO);
                 }
 
-        
+
                 // A bola nao esta em nossa area
-                // A seguir sao calculos para evitar que o volante entre na area                                                                        
+                // A seguir sao calculos para evitar que o volante entre na area
                 else {
 
                     // A bola esta em uma faixa acima da area
@@ -77,7 +77,7 @@ void Roteiro::volante(Robo& r, posXY& b) {
 
                         // A bola esta a frente da area
                         if (b.x > TAM_X_AREA_GOL){
-                            // O robo fica na "quina da area"                                                                                        
+                            // O robo fica na "quina da area"
                             r.setPosicaoObjX(TAM_X_AREA_GOL + TAM_ROBO / 2);
                         }
                         // A bola esta encima da area
@@ -89,9 +89,9 @@ void Roteiro::volante(Robo& r, posXY& b) {
                     // A bola esta em uma faixa abaixo da area
                     else if (b.y < TAM_Y_CAMPO / 2 - TAM_Y_AREA_GOL / 2) {
 
-                        
+
                         r.setPosicaoObjY(TAM_Y_CAMPO / 2 - TAM_Y_AREA_GOL / 2 - TAM_ROBO / 2);
-                        
+
                         // A bola esta a frente da area
                         if (b.x > TAM_X_AREA_GOL){
                             // O robo fica na "quina da area"
@@ -108,7 +108,7 @@ void Roteiro::volante(Robo& r, posXY& b) {
                     else {
                         // O robo fica na linha de frente da area
                         Comportamento::posicionaPosBolaEmY(r, b);
-                        r.setPosicaoObjX(TAM_X_AREA_GOL + TAM_ROBO / 2);                                                         
+                        r.setPosicaoObjX(TAM_X_AREA_GOL + TAM_ROBO / 2);
                     }
                 }
             }
