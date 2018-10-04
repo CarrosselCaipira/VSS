5fdada4e465e5bd2339897fd6bf1cade8a75c460
MarceloGNunez <marcelo.nunez.zcfi@gmail.com>
2018-10-03 22:03:23 -0300

    Uso melhor das funções de propositoGeral e uso correto do bitset para efetuar o chute girando

--- atacante.cpp
+++ atacante.cpp
@@ -4,6 +4,16 @@
 //bool Tecnico.setEstrategiaAtacanteRetornou() => muda uma variavel bool no tecnico
 void Roteiro::atacante(Robo& r, posXY& b){
 
+    // Se o atacante ja estiver realizando o chute girando no sentido horario
+    if(r.atributos.test(CHUTE_GIRANDO_HORARIO)) {
+		r.atributos.reset(CHUTE_GIRANDO_HORARIO);
+	}
+
+	// Se o atacante ja estiver realizando o chute girando no sentido anti horario
+	else if(r.atributos.test(CHUTE_GIRANDO_ANTI_HORARIO)) {
+		r.atributos.reset(CHUTE_GIRANDO_ANTI_HORARIO);
+	}
+
     // Se a bola estiver no quadrado inutil e o robo estiver perto
     if(b.isInQuadradosInuteis() && r.getPosicaoAtualRobo().isInRaio(b, TAM_ROBO/2)){
         // Se estiver em algum quadrado inutil superior, chute girando horario
@@ -18,6 +28,9 @@ void Roteiro::atacante(Robo& r, posXY& b){
 
     // Se o robo estiver com a bola
     else if(r.isRoboEmPosseBola(b)){
+        // Seta o bitset para a posse da bola
+        r.atributos.set(EM_POSSE_BOLA);
+
         // Se a bola estiver logo em frente ao robo
         if(b.x >= r.getPosicaoAtualRobo().x){
             Comportamento::posicionaCentroGolXYAdv(r);
@@ -34,8 +47,11 @@ void Roteiro::atacante(Robo& r, posXY& b){
     // O atacante nao esta com a bola
     // Se a bola estiver no campo adversario
     else if (b.isInCampoAdv()){
+        // Reseta o bitset para a posse da bola
+        r.atributos.reset(EM_POSSE_BOLA);
+
         // O atacante volta para o meio de campo e depois avanca para cima da bola
-        if(r.getPosicaoAtualRobo().x > TAM_X_CAMPO/2 && !Tecnico.getEstrategiaAtacanteRetornou()){
+        if(r.getPosicaoAtualRobo().isInCampoAdv() && !Tecnico.getEstrategiaAtacanteRetornou()){
             Comportamento::posicionaCentroCampoX(r);
         }
 
@@ -52,6 +68,9 @@ void Roteiro::atacante(Robo& r, posXY& b){
     
     // Se a bola esta no nosso campo
     else if (b.isInCampoTime()){
+        // Reseta o bitset para a posse da bola
+        r.atributos.reset(EM_POSSE_BOLA);
+
         // O atacante avanca na bola, porem nao entra na nossa area
         if(b.isInAreaGolTime()){
             Comportamento::permaneceNoEixoX(r);
