07f095d6d43f2a1d0df811c8b3d16056f84b9f26
MarceloGNunez <marcelo.nunez.zcfi@gmail.com>
2018-10-03 22:14:18 -0300

    Criadas variaveis e definidas funções que serão utilizadas pelo tecnicoOfensivo

--- tecnicoOfensivo.hpp
+++ tecnicoOfensivo.hpp
@@ -1,17 +1,25 @@
-#ifndef TECNICO_OFENCIVO_H
+#ifndef TECNICO_OFENSIVO_H
 #include "tecnico.hpp"
+#include "roteiro.hpp"
 
 class TecnicoOfensivo : public Tecnico {
   private:
+    short int indiceAtacante;
+    short int indiceVolante;
+    short int indiceGoleiro;
+    bool atacanteRetornou;
+
+  public:
+    TecnicoOfensivo(std::vector<Robo>& r, std::vector<Robo>& rAdv, Bola& b); // possivel troca de Bola& b por posXY& b
+    TecnicoOfensivo(std::vector<Robo>& r, std::vector<posXY>& rAdv, posXY& b);
+
     bool isAtacantePosse();
     posXY getPosAtualAtacante();
     void permutaRoteiroAtkVol();
-
-  public:
-    TecnicoOfensivo(std::vector<Robo>& r, std::vector<Robo>& rAdv, Bola& b);
-    TecnicoOfensivo(std::vector<Robo>& r, std::vector<posXY>& rAdv, posXY& b);
+    void setEstrategiaAtacanteRetornou(bool set);
+    bool getEstrategiaAtacanteRetornou();
 
     run();
 };
 
-# endif /* TECNICO_OFENCIVO_H */
+# endif /* TECNICO_OFENSIVO_H */
